d753c2cb20405604db7ef1ef930d57c29871fa8e
yayayahei <mmwcbz@msn.cn>
2018-01-15 23:14:51 +0800

    add plugin dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,256 +3,288 @@
  */
 
 /* jshint node: true */
-module.exports = function(grunt) {
-	'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-	// Force use of Unix newlines
-	grunt.util.linefeed = '\n';
+    // Force use of Unix newlines
+    grunt.util.linefeed = '\n';
 
-	RegExp.quote = function(string) {
-		return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-	};
+    RegExp.quote = function (string) {
+        return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+    };
 
-	var generateNamespace = require('./grunt/mui-namespace-generator.js');
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+    var generateNamespace = require('./grunt/mui-namespace-generator.js');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		// Metadata.
-		meta: {
-			libPath: 'lib/',
-			distPath: 'dist/',
-			jsPath: 'js/',
-			sassPath: 'sass/',
-			examplesPath: 'examples/hello-mui/'
-		},
+        // Metadata.
+        meta: {
+            libPath: 'lib/',
+            distPath: 'dist/',
+            jsPath: 'js/',
+            sassPath: 'sass/',
+            examplesPath: 'examples/hello-mui/',
+            pluginPickerPath: 'plugin/picker/',
+            pluginPickerDistPath: 'plugin/picker/dist/',
+        },
 
-		banner: '/*!\n' +
-			' * =====================================================\n' +
-			' * Mui v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-			' * =====================================================\n' +
-			' */\n',
+        banner: '/*!\n' +
+        ' * =====================================================\n' +
+        ' * Mui v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+        ' * =====================================================\n' +
+        ' */\n',
 
-		clean: {
-			all: ['<%= meta.distPath %>'],
-			sourceMap: ['<%= meta.distPath %>css/*.map']
-		},
+        clean: {
+            all: ['<%= meta.distPath %>'],
+            sourceMap: ['<%= meta.distPath %>css/*.map']
+        },
 
-		concat: {
-			mui: {
-				options: {
-					banner: '<%= banner %>'
-				},
-				src: [
-					'js/mui.js',
-					'js/mui.detect.js',
-					'js/mui.detect.5+.js',
-					'js/mui.event.js',
-					'js/mui.target.js',
-					'js/mui.fixed.js',
-					'js/mui.fixed.bind.js',
-					'js/mui.fixed.classlist.js',
-					'js/mui.fixed.animation.js',
-					'js/mui.fixed.fastclick.js',
-					'js/mui.fixed.keyboard.js',
-					'js/mui.namespace.js',
-					'js/mui.gestures.js',
-					'js/mui.gestures.flick.js',
-					'js/mui.gestures.swipe.js',
-					'js/mui.gestures.drag.js',
-					'js/mui.gestures.tap.js',
-					'js/mui.gestures.longtap.js',
-					'js/mui.gestures.hold.js',
-					'js/mui.gestures.pinch.js',
-					'js/mui.init.js',
-					'js/mui.init.5+.js',
-					'js/mui.back.js',
-					'js/mui.back.5+.js',
-					'js/mui.init.pullrefresh.js',
-					'js/mui.ajax.js',
-					'js/mui.ajax.5+.js',
-					'js/mui.layout.js',
-					'js/mui.animation.js',
-					'js/mui.class.js',
-					'js/mui.pullrefresh.js',
-					'js/mui.class.scroll.js',
-					'js/mui.class.scroll.pullrefresh.js',
-					'js/mui.class.scroll.slider.js',
-					'js/mui.pullrefresh.5+.js',
-					'js/mui.offcanvas.js',
-					'js/actions.js',
-					'js/modals.js',
-					'js/popovers.js',
-					'js/segmented-controllers.js',
-					'js/switches.js',
-					'js/tableviews.js',
-					'js/mui.dialog.alert.js',
-					'js/mui.dialog.confirm.js',
-					'js/mui.dialog.prompt.js',
-					'js/mui.dialog.toast.js',
-					'js/mui.popup.js',
-					'js/mui.progressbar.js',
-					'js/input.plugin.js',
-					'js/mui.transparent.js',
-					'js/mui.number.js',
-					'js/mui.button.js'
-				],
-				dest: '<%= meta.distPath %>js/<%= pkg.name %>.js',
-			}
-		},
+        concat: {
+            mui: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                src: [
+                    'js/mui.js',
+                    'js/mui.detect.js',
+                    'js/mui.detect.5+.js',
+                    'js/mui.event.js',
+                    'js/mui.target.js',
+                    'js/mui.fixed.js',
+                    'js/mui.fixed.bind.js',
+                    'js/mui.fixed.classlist.js',
+                    'js/mui.fixed.animation.js',
+                    'js/mui.fixed.fastclick.js',
+                    'js/mui.fixed.keyboard.js',
+                    'js/mui.namespace.js',
+                    'js/mui.gestures.js',
+                    'js/mui.gestures.flick.js',
+                    'js/mui.gestures.swipe.js',
+                    'js/mui.gestures.drag.js',
+                    'js/mui.gestures.tap.js',
+                    'js/mui.gestures.longtap.js',
+                    'js/mui.gestures.hold.js',
+                    'js/mui.gestures.pinch.js',
+                    'js/mui.init.js',
+                    'js/mui.init.5+.js',
+                    'js/mui.back.js',
+                    'js/mui.back.5+.js',
+                    'js/mui.init.pullrefresh.js',
+                    'js/mui.ajax.js',
+                    'js/mui.ajax.5+.js',
+                    'js/mui.layout.js',
+                    'js/mui.animation.js',
+                    'js/mui.class.js',
+                    'js/mui.pullrefresh.js',
+                    'js/mui.class.scroll.js',
+                    'js/mui.class.scroll.pullrefresh.js',
+                    'js/mui.class.scroll.slider.js',
+                    'js/mui.pullrefresh.5+.js',
+                    'js/mui.offcanvas.js',
+                    'js/actions.js',
+                    'js/modals.js',
+                    'js/popovers.js',
+                    'js/segmented-controllers.js',
+                    'js/switches.js',
+                    'js/tableviews.js',
+                    'js/mui.dialog.alert.js',
+                    'js/mui.dialog.confirm.js',
+                    'js/mui.dialog.prompt.js',
+                    'js/mui.dialog.toast.js',
+                    'js/mui.popup.js',
+                    'js/mui.progressbar.js',
+                    'js/input.plugin.js',
+                    'js/mui.transparent.js',
+                    'js/mui.number.js',
+                    'js/mui.button.js'
+                ],
+                dest: '<%= meta.distPath %>js/<%= pkg.name %>.js',
+            }
+        },
 
-		sass: {
-			options: {
-				banner: '<%= banner %>',
-				style: 'expanded',
-				unixNewlines: true
-			},
-			dist: {
-				files: {
-					'<%= meta.distPath %>css/<%= pkg.name %>.css': 'sass/mui.scss',
-				}
-			}
-		},
+        sass: {
+            options: {
+                banner: '<%= banner %>',
+                style: 'expanded',
+                unixNewlines: true
+            },
+            dist: {
+                files: {
+                    '<%= meta.distPath %>css/<%= pkg.name %>.css': 'sass/mui.scss',
+                }
+            }
+        },
 
-		csscomb: {
-			options: {
-				config: 'sass/.csscomb.json'
-			},
-			dist: {
-				files: {
-					'<%= meta.distPath %>/css/<%= pkg.name %>.css': '<%= meta.distPath %>/css/<%= pkg.name %>.css'
-				}
-			},
-		},
+        csscomb: {
+            options: {
+                config: 'sass/.csscomb.json'
+            },
+            dist: {
+                files: {
+                    '<%= meta.distPath %>/css/<%= pkg.name %>.css': '<%= meta.distPath %>/css/<%= pkg.name %>.css'
+                }
+            },
+        },
+        copy: {
+            fonts: {
+                expand: true,
+                src: 'fonts/mui*.ttf',
+                dest: '<%= meta.distPath %>/'
+            },
+            examples: {
+                expand: true,
+                cwd: '<%= meta.distPath %>',
+                src: ['**/mui*'],
+                dest: '<%= meta.examplesPath %>'
+            },
+            plugin: {
+                expand: true,
+                cwd: '<%= meta.pluginPickerDistPath %>',
+                src: ['**/mui*'],
+                dest: '<%= meta.examplesPath %>'
+            }
+        },
 
-		copy: {
-			fonts: {
-				expand: true,
-				src: 'fonts/mui*.ttf',
-				dest: '<%= meta.distPath %>/'
-			},
-			examples: {
-				expand: true,
-				cwd: '<%= meta.distPath %>',
-				src: ['**/mui*'],
-				dest: '<%= meta.examplesPath %>'
-			}
-		},
+        cssmin: {
+            options: {
+                banner: '', // set to empty; see bellow
+                keepSpecialComments: '*', // set to '*' because we already add the banner in sass
+                sourceMap: false
+            },
+            mui: {
+                src: '<%= meta.distPath %>css/<%= pkg.name %>.css',
+                dest: '<%= meta.distPath %>css/<%= pkg.name %>.min.css'
+            }
+        },
 
-		cssmin: {
-			options: {
-				banner: '', // set to empty; see bellow
-				keepSpecialComments: '*', // set to '*' because we already add the banner in sass
-				sourceMap: false
-			},
-			mui: {
-				src: '<%= meta.distPath %>css/<%= pkg.name %>.css',
-				dest: '<%= meta.distPath %>css/<%= pkg.name %>.min.css'
-			}
-		},
+        uglify: {
+            options: {
+                banner: '<%= banner %>',
+                compress: {},
+                mangle: true,
+                preserveComments: false
+            },
+            mui: {
+                src: '<%= concat.mui.dest %>',
+                dest: '<%= meta.distPath %>js/<%= pkg.name %>.min.js'
+            }
+        },
+        shell: {
+            // cwd: '<%= meta.pluginPickerPath %>',
+            buildPicker: {
+                command: 'gulp default',
+                options:{
+                    execOptions: {
+                        cwd: '<%= meta.pluginPickerPath %>',
+                    }
+                },
+            }
+        },
+        watch: {
+            options: {
+                dateFormat: function (time) {
+                    grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+                    grunt.log.writeln('Waiting for more changes...');
+                },
+                livereload: true
+            },
+            scripts: {
+                files: [
+                    '<%= meta.sassPath %>/**/*.scss',
+                    '<%= meta.jsPath %>/**/*.js',
+                    '<%= meta.pluginPickerPath %>js/*.js',
+                    '<%= meta.pluginPickerPath %>css/*.css',
+                ],
+                tasks: 'dist'
+            },
+            pluginPicker:{
+                files:[
+                    '<%= meta.pluginPickerPath %>js/*.js',
+                    '<%= meta.pluginPickerPath %>css/*.css',
+                ],
+                tasks:['dist-plugin'],
+                options: {
+                    spawn: false
+                },
+            }
+        },
 
-		uglify: {
-			options: {
-				banner: '<%= banner %>',
-				compress: {},
-				mangle: true,
-				preserveComments: false
-			},
-			mui: {
-				src: '<%= concat.mui.dest %>',
-				dest: '<%= meta.distPath %>js/<%= pkg.name %>.min.js'
-			}
-		},
+        jshint: {
+            options: {
+                jshintrc: 'js/.jshintrc'
+            },
+            grunt: {
+                src: ['Gruntfile.js', 'grunt/*.js']
+            },
+            src: {
+                src: 'js/*.js'
+            }
+        },
 
-		watch: {
-			options: {
-				dateFormat: function(time) {
-					grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
-					grunt.log.writeln('Waiting for more changes...');
-				},
-				livereload: true
-			},
-			scripts: {
-				files: [
-					'<%= meta.sassPath %>/**/*.scss',
-					'<%= meta.jsPath %>/**/*.js',
-				],
-				tasks: 'dist'
-			}
-		},
+        jscs: {
+            options: {
+                config: 'js/.jscsrc'
+            },
+            grunt: {
+                src: '<%= jshint.grunt.src %>'
+            },
+            src: {
+                src: '<%= jshint.src.src %>'
+            },
+            docs: {
+                src: '<%= jshint.docs.src %>'
+            }
+        },
 
-		jshint: {
-			options: {
-				jshintrc: 'js/.jshintrc'
-			},
-			grunt: {
-				src: ['Gruntfile.js', 'grunt/*.js']
-			},
-			src: {
-				src: 'js/*.js'
-			}
-		},
+        csslint: {
+            options: {
+                csslintrc: 'sass/.csslintrc'
+            },
+            src: [
+                '<%= meta.distPath %>/css/<%= pkg.name %>.css',
+            ]
+        },
+        sed: {
+            versionNumber: {
+                pattern: (function () {
+                    var old = grunt.option('oldver');
+                    return old ? RegExp.quote(old) : old;
+                })(),
+                replacement: grunt.option('newver'),
+                recursive: true
+            }
+        }
+    });
+    // Load the plugins
+    require('load-grunt-tasks')(grunt, {
+        scope: 'devDependencies'
+    });
+    require('time-grunt')(grunt);
 
-		jscs: {
-			options: {
-				config: 'js/.jscsrc'
-			},
-			grunt: {
-				src: '<%= jshint.grunt.src %>'
-			},
-			src: {
-				src: '<%= jshint.src.src %>'
-			},
-			docs: {
-				src: '<%= jshint.docs.src %>'
-			}
-		},
-
-		csslint: {
-			options: {
-				csslintrc: 'sass/.csslintrc'
-			},
-			src: [
-				'<%= meta.distPath %>/css/<%= pkg.name %>.css',
-			]
-		},
-		sed: {
-			versionNumber: {
-				pattern: (function() {
-					var old = grunt.option('oldver');
-					return old ? RegExp.quote(old) : old;
-				})(),
-				replacement: grunt.option('newver'),
-				recursive: true
-			}
-		}
-	});
-	// Load the plugins
-	require('load-grunt-tasks')(grunt, {
-		scope: 'devDependencies'
-	});
-	require('time-grunt')(grunt);
-	// Default task(s).
-	grunt.registerTask('cleanAll', ['clean']);
-	grunt.registerTask('dist-css', ['sass', 'csscomb', 'cssmin', 'clean:sourceMap']);
-	grunt.registerTask('dist-js', ['concat', 'build-namespace', 'uglify']);
-	grunt.registerTask('dist', ['clean:all', 'dist-css', 'dist-js', 'copy']);
-	grunt.registerTask('build', ['dist']);
-	grunt.registerTask('default', ['dist']);
+    // Default task(s).
+    grunt.registerTask('cleanAll', ['clean']);
+    grunt.registerTask('dist-css', ['sass', 'csscomb', 'cssmin', 'clean:sourceMap']);
+    grunt.registerTask('dist-js', ['concat', 'build-namespace', 'uglify']);
+    grunt.registerTask('build-plugin', ['shell:buildPicker']);
+    grunt.registerTask('dist-plugin', ['build-plugin','copy:plugin']);
+    grunt.registerTask('dist', ['clean:all', 'dist-css', 'dist-js', 'build-plugin', 'copy']);
+    grunt.registerTask('build', ['dist']);
+    grunt.registerTask('default', ['dist']);
 
 
-	grunt.registerTask('build-namespace', generateNamespace);
+    grunt.registerTask('build-namespace', generateNamespace);
 
-	grunt.registerTask('server', ['dist','watch']);
+    grunt.registerTask('server', ['dist', 'watch']);
+    grunt.registerTask('server-plugin', ['dist-plugin', 'watch:pluginPicker',]);
 
 
+    // Version numbering task.
+    // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
+    // This can be overzealous, so its changes should always be manually reviewed!
+    grunt.registerTask('change-version-number', 'sed');
 
-	// Version numbering task.
-	// grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
-	// This can be overzealous, so its changes should always be manually reviewed!
-	grunt.registerTask('change-version-number', 'sed');
-
-	grunt.event.on('watch', function(action, filepath, target) {
-		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
-	});
+    grunt.event.on('watch', function (action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
 };
--- examples/hello-mui/js/mui.picker.all.js
+++ examples/hello-mui/js/mui.picker.all.js
@@ -1016,3 +1016,207 @@
 	});
 
 })(mui, document);
+/**
+ * 弹出选择列表插件
+ * 此组件依赖 listpcker ，请在页面中先引入 mui.picker.css + mui.picker.js
+ * varstion 1.0.0
+ * by yayayahei
+ * mmwcbz@msn.cn
+ */
+
+(function ($, document) {
+
+    //创建 DOM
+    $.dom = function (str) {
+        if (typeof(str) !== 'string') {
+            if ((str instanceof Array) || (str[0] && str.length)) {
+                return [].slice.call(str);
+            } else {
+                return [str];
+            }
+        }
+        if (!$.__create_dom_div__) {
+            $.__create_dom_div__ = document.createElement('div');
+        }
+        $.__create_dom_div__.innerHTML = str;
+        return [].slice.call($.__create_dom_div__.childNodes);
+    };
+
+    var panelBuffer = '<div class="mui-poppicker">\
+		<div class="mui-poppicker-header">\
+			<button class="mui-btn mui-poppicker-btn-cancel">取消</button>\
+			<button class="mui-btn mui-btn-blue mui-poppicker-btn-ok">确定</button>\
+			<div class="mui-poppicker-clear"></div>\
+		</div>\
+		<div class="mui-poppicker-title"></div>\
+		<div class="mui-poppicker-body">\
+		</div>\
+	</div>';
+    var titleBuffer = '<h5 data-id="title">请选择</h5>';
+    var pickerBuffer = '<div class="mui-picker">\
+		<div class="mui-picker-inner">\
+			<div class="mui-pciker-rule mui-pciker-rule-ft"></div>\
+			<ul class="mui-pciker-list">\
+			</ul>\
+			<div class="mui-pciker-rule mui-pciker-rule-bg"></div>\
+		</div>\
+	</div>';
+
+    //定义弹出选择器类
+    var MultiLevelPopPicker = $.MultiLevelPopPicker = $.Class.extend({
+        //构造函数
+        init: function (options) {
+            var self = this;
+            self.options = options || {};
+            self.options.buttons = self.options.buttons || ['取消', '确定'];
+            self.panel = $.dom(panelBuffer)[0];
+            document.body.appendChild(self.panel);
+            self.ok = self.panel.querySelector('.mui-poppicker-btn-ok');
+            self.cancel = self.panel.querySelector('.mui-poppicker-btn-cancel');
+            self.title = self.panel.querySelector('.mui-poppicker-title');
+            self.body = self.panel.querySelector('.mui-poppicker-body');
+            self.mask = $.createMask();
+            self.cancel.innerText = self.options.buttons[0];
+            self.ok.innerText = self.options.buttons[1];
+            self.cancel.addEventListener('tap', function (event) {
+                self.hide();
+            }, false);
+            self.ok.addEventListener('tap', function (event) {
+                if (self.callback) {
+                    var rs = self.callback(self.getSelectedItems());
+                    if (rs !== false) {
+                        self.hide();
+                    }
+                }
+            }, false);
+            self.mask[0].addEventListener('tap', function () {
+                self.hide();
+            }, false);
+            self._createPicker();
+            // //防止滚动穿透
+            // self.panel.addEventListener($.EVENT_START, function (event) {
+            //     event.preventDefault();
+            // }, false);
+            // self.panel.addEventListener($.EVENT_MOVE, function (event) {
+            //     event.preventDefault();
+            // }, false);
+        },
+        _createPicker: function () {
+            var self = this;
+            var layer = self.options.layer || 1;
+            var width = '100%';
+            self.pickers = [];
+            self.titles = [];
+            self.pickerElements = [];
+            for (var i = 1; i <= layer; i++) {
+                var pickerElement = $.dom(pickerBuffer)[0];
+                pickerElement.setAttribute("data-id", i);
+                pickerElement.style.width = width;
+                // append title
+                var titleElement = $.dom(titleBuffer)[0];
+                console.log(titleElement);
+                titleElement.setAttribute("data-id", i);
+                if (i === 1) {
+                    titleElement.classList.add('active');
+                    pickerElement.classList.add('active');
+                }
+                titleElement.addEventListener('tap', function (event) {
+                    var id = Number(this.getAttribute("data-id"));
+                    var index = Number(this.getAttribute("data-value"));
+                  var prevIndex=  this.previousSibling&&Number(this.previousSibling.getAttribute("data-value"));
+                    console.log(prevIndex);
+                    for (var _id = 0; _id < layer; _id++) {
+                        if (_id + 1 === id) {
+                            // active this title
+                            self.pickerElements[_id].classList.add('active');
+                            // display corresponding picker,
+                            self.titles[_id].classList.add('active');
+
+                        } else {
+                            // deactive other picker
+                            self.pickerElements[_id].classList.remove('active');
+                            // deactive other title
+                            self.titles[_id].classList.remove('active');
+
+                        }
+                    }
+                    if(id>1){
+                        self.pickers[id - 1].triggerChange();
+                    }
+                }, false);
+                self.title.appendChild(titleElement);
+                self.titles.push(titleElement);
+                self.pickerElements.push(pickerElement);
+                self.body.appendChild(pickerElement);
+                var picker = $(pickerElement).picker();
+                self.pickers.push(picker);
+                pickerElement.addEventListener('change', function (event) {
+                    var id = Number(this.getAttribute("data-id"));
+                    console.log(event);
+                    var eventData = event.detail || {};
+                    var preItem = eventData.item || {};
+                    var thisTitleElement = self.titles[id - 1];
+                    thisTitleElement.innerText = preItem.text;
+                    thisTitleElement.setAttribute('data-value', eventData.index);
+                    var nextPickerElement = this.nextSibling;
+                    if (nextPickerElement && nextPickerElement.picker) {
+                        nextPickerElement.picker.setItems(preItem.children);
+                    }
+                }, false);
+            }
+        },
+        //填充数据
+        setData: function (data) {
+            var self = this;
+            data = data || [];
+            self.pickers[0].setItems(data);
+        },
+        //获取选中的项（数组）
+        getSelectedItems: function () {
+            var self = this;
+            var items = [];
+            for (var i in self.pickers) {
+                var picker = self.pickers[i];
+                items.push(picker.getSelectedItem() || {});
+            }
+            return items;
+        },
+        //显示
+        show: function (callback) {
+            var self = this;
+            self.callback = callback;
+            self.mask.show();
+            document.body.classList.add($.className('poppicker-active-for-page'));
+            self.panel.classList.add($.className('active'));
+            //处理物理返回键
+            self.__back = $.back;
+            $.back = function () {
+                self.hide();
+            };
+        },
+        //隐藏
+        hide: function () {
+            var self = this;
+            if (self.disposed) return;
+            self.panel.classList.remove($.className('active'));
+            self.mask.close();
+            document.body.classList.remove($.className('poppicker-active-for-page'));
+            //处理物理返回键
+            $.back = self.__back;
+        },
+        dispose: function () {
+            var self = this;
+            self.hide();
+            setTimeout(function () {
+                self.panel.parentNode.removeChild(self.panel);
+                for (var name in self) {
+                    self[name] = null;
+                    delete self[name];
+                }
+                ;
+                self.disposed = true;
+            }, 300);
+        }
+    });
+
+})(mui, document);
